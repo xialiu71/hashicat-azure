f4e89cfcd4c00f233fa292d1c1b9a8b071f1a93c
Xia Liu <xialiu1998@gmail.com>
2023-01-19 21:33:33 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -2,5 +2,5 @@
 module "network" {
   source  = "app.terraform.io/xl-test-org/network/azurerm"
   version = "3.5.0"
-  esource_group_name = azurerm_resource_group.myresourcegroup.name
+  resource_group_name = azurerm_resource_group.myresourcegroup.name
 }
