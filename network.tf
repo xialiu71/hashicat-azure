a4cdfb21960a9942778f85ef78c780e5be777cf9
Xia Liu <xialiu1998@gmail.com>
2023-01-19 21:32:38 +0000

    Added network module

--- network.tf
+++ network.tf
@@ -1,20 +1,6 @@
+
 module "network" {
-  source              = "xialiu71/network/azurerm"
-  resource_group_name = azurerm_resource_group.myresourcegroup.name
-  address_spaces      = ["10.0.0.0/16", "10.2.0.0/16"]
-  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
-  subnet_names        = ["subnet1", "subnet2", "subnet3"]
-
-  subnet_service_endpoints = {
-    "subnet1" : ["Microsoft.Sql"], 
-    "subnet2" : ["Microsoft.Sql"],
-    "subnet3" : ["Microsoft.Sql"]
-  }
-
-  tags = {
-    environment = "dev"
-    costcenter  = "it"
-  }
-
-  depends_on = [azurerm_resource_group.example]
+  source  = "app.terraform.io/xl-test-org/network/azurerm"
+  version = "3.5.0"
+  esource_group_name = azurerm_resource_group.myresourcegroup.name
 }
